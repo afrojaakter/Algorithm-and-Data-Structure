641ca3e65ab9471558a50121ed4cc55972210efd
Afroja Akter <46405612+afrojaakter@users.noreply.github.com>
2020-12-24 16:17:20 -0600

    Update Max Consecutive Ones.py

--- LeetCode/Max Consecutive Ones.py
+++ LeetCode/Max Consecutive Ones.py
@@ -1,4 +1,12 @@
 #Question: Given a binary array, find the maximum number of consecutive 1s in this array.
+'''
+Example:
+Input: [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s.
+    The maximum number of consecutive 1s is 3.
+'''
+    
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
         count_ones = 0
