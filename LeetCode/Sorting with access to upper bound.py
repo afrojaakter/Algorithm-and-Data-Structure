8c3500fea0d9fbea87e4a216ce3d64bdd0dc0f34
Afroja Akter <46405612+afrojaakter@users.noreply.github.com>
2020-12-26 17:57:22 -0600

    Update Sorting with access to upper bound.py

--- LeetCode/Sorting with access to upper bound.py
+++ LeetCode/Sorting with access to upper bound.py
@@ -1,3 +1,27 @@
+'''
+#This question is taken from the course Interview cake: 
+https://www.interviewcake.com/question/python3/top-scores?course=fc1&section=sorting-searching-logarithms
+You created a game that is more popular than Angry Birds.
+
+Each round, players receive a score between 0 and 100, which you use to rank them from highest to lowest. 
+So far you're using an algorithm that sorts in O(n\lg{n})O(nlgn) time, but players are complaining that their 
+rankings aren't updated fast enough. You need a faster sorting algorithm.
+
+Write a function that takes:
+
+a list of unsorted_scores
+the highest_possible_score in the game
+and returns a sorted list of scores in less than O(n\lg{n})O(nlgn) time.
+
+For example:
+
+  unsorted_scores = [37, 89, 41, 65, 91, 53]
+HIGHEST_POSSIBLE_SCORE = 100
+
+# Returns [91, 89, 65, 53, 41, 37]
+sort_scores(unsorted_scores, HIGHEST_POSSIBLE_SCORE)
+'''
+
 def sort_scores(unsorted_scores, highest_possible_score):
     # Sort the scores in O(n) time
     # List with length of hightest Possible Score
